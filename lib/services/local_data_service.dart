9559a2ac591051a01fd94d916626377159d6f963
webskydavid <webskydavid@gmail.com>
2020-10-08 22:56:27 +0200

    Add method in LocalDataService

--- lib/services/local_data_service.dart
+++ lib/services/local_data_service.dart
@@ -1,1 +1,12 @@
-class LocalDataService {}
+import 'package:app/models/models.dart';
+import 'package:hive/hive.dart';
+
+class LocalDataService {
+  Box<List<ExerciseModel>> exerciseBox;
+
+  void init() async {
+    await Hive.openBox('20201010');
+  }
+
+  void getExercisesBy(String day) {}
+}
